2c47609aec4c60af9ea8b7ab73801b6e10ce9ed9
zhazha-18 <my_king6@163.com>
2020-06-29 16:28:08 +0800

    创建

---
+++ RegisterCenter/src/main/java/com/rongyi/register/BaiseeRegisterApplication.java
@@ -0,0 +1,15 @@
+package com.rongyi.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer//表明这是注册中心  标注为注册中心服务
+public class BaiseeRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaiseeRegisterApplication.class, args);
+	}
+
+}
